e2af773750388480c890ed737b000feacac3ac27
Andrew Blum <andrew.blum@gmail.com>
2015-10-08 17:21:41 -0700

    add refleciton

--- week-5/gps2_2.rb
+++ week-5/gps2_2.rb
@@ -37,11 +37,23 @@ end
 
 shopping_list = new_list
 add_item(shopping_list, "Lemonade", 2)
-add_item(shopping_list, "Tomotoes", 3)
+add_item(shopping_list, "Tomatoes", 3)
 add_item(shopping_list, "Onions", 1)
 add_item(shopping_list, "Ice Cream", 4)
 print_list(shopping_list)
 remove_item(shopping_list, "Lemonade")
 update_item(shopping_list, "Ice Cream", 1)
 print_list(shopping_list)
+# What did you learn about pseudocode from working on this challenge?
+# => Sadly I didn't do pseudocode for this exercise and I most definitely should have. I am working hard to make sure that I am pseudocoding for challenges to come. I think the best part of pseudocoding is that allows for the solution to broken down line by line. So that when you go to translate it you aren't really thinking about the logic, but just what methods to use.
+# What are the tradeoffs of using Arrays and Hashes for this challenge?
+# => While both could most likely be used, my partner and I found that it would make more sense for us to use a hash. The reason we used a hash is because, the key is the item, and qty is the value.
+# What does a method return?
+# => A method returns the last evaluated. unless explicitly told to return something else.
+# What kind of things can you pass into methods as arguments?
+# => You can pass almost anything in to a method. Including another method.
+# How can you pass information between methods?
+# You can assign what a method returns into a variable, then pass that variable into the other method.
+# What concepts were solidified in this challenge, and what concepts are still confusing?
+# => I think there were two things that I feel comfortable with now. The first being, a method that just returns an empty hash. For a little bit during the GPS, I couldn't remember how to make a hash. Thanks to our guide, I don't think there was anything that I am still not understanding in this exercise
 
