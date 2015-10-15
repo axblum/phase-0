83ca5bfafdf80250d1a9adf364f464dfba3253bc
Andrew Blum <andrew.blum@gmail.com>
2015-10-15 16:13:05 -0700

    add refactoring GPS

--- week-6/gps2_3.rb
+++ week-6/gps2_3.rb
@@ -6,38 +6,28 @@
 
 # Bakery Serving Size portion calculator.
 
-def serving_size_calc(item_to_make, order_quantity)
+def serving_size_calc(item_to_make, num_people)
   library = {"cookie" => 1, "cake" =>  5, "pie" => 7}
-  error_counter = 3
 
-  library.each do |food|
-    if library[food] != library[item_to_make]
-      p error_counter += -1
-    end
-  end
+    raise ArgumentError.new("#{item_to_make} is not a valid input") if library.has_key?(item_to_make) == false
 
-  if error_counter > 0
-    raise ArgumentError.new("#{item_to_make} is not a valid input")
-  end
+  serving_size = library.fetch(item_to_make)
 
-  serving_size = library.values_at(item_to_make)[0]
-  serving_size_mod = order_quantity % serving_size
-
-  case serving_size_mod
-  when 0
-    return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}"
+  if num_people % serving_size == 0
+    return "Calculations complete: Make #{num_people/serving_size} of #{item_to_make}"
   else
-    return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you have #{serving_size_mod} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
+    return "Calculations complete: Make #{(num_people.to_f/serving_size.to_f).ceil} of #{item_to_make}, you have #{serving_size - (num_people % serving_size)} leftover servings."
   end
 end
 
 p serving_size_calc("pie", 7)
 p serving_size_calc("pie", 8)
+p serving_size_calc("pie",5)
 p serving_size_calc("cake", 5)
 p serving_size_calc("cake", 7)
 p serving_size_calc("cookie", 1)
 p serving_size_calc("cookie", 10)
-p serving_size_calc("THIS IS AN ERROR", 5)
+#p serving_size_calc("THIS IS AN ERROR", 5)
 
 #  Reflection
 
